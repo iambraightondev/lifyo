5c86f094925b050b224fc920721c5f02b78ff140
Braighton Paico <braightonfoulman@gmail.com>
2025-10-06 18:40:36 -0500

    first commit

---
+++ src/main/java/com/lifyo/LifyoApplication.java
@@ -0,0 +1,13 @@
+package com.lifyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifyo/LifyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
